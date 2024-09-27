dd8274adb3ea240e53cfe3845847054a315a0dc0
Yogihafidh <hafidhyogi@gmail.com>
2024-09-27 21:12:30 +0700

    refactor(bank): add try catch feature

--- F-BEE24001186-km7-YHM-bankingsystem-ch2/banking_system.js
+++ F-BEE24001186-km7-YHM-bankingsystem-ch2/banking_system.js
@@ -2,6 +2,17 @@ const { Bank, rl } = require("./bank_account");
 
 async function main() {
   const rekening = new Bank();
+
+  try {
+    const deposit = await rekening.deposit();
+    console.log(deposit);
+    const withdraw = await rekening.withdraw();
+    console.log(withdraw);
+  } catch (err) {
+    console.error(err.message);
+  } finally {
+    rl.close(); // Menutup interface setelah seluruh operasi selesai
+  }
 }
 
 main();
