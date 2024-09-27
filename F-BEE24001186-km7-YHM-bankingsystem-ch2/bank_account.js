4b14e21895b70f7d445928e6bc6ba49e8514dc2d
Yogihafidh <hafidhyogi@gmail.com>
2024-09-27 20:54:00 +0700

    feat(bank): create Bank class

--- F-BEE24001186-km7-YHM-bankingsystem-ch2/bank_account.js
+++ F-BEE24001186-km7-YHM-bankingsystem-ch2/bank_account.js
@@ -1,19 +1,5 @@
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 class Bank {
   constructor(saldoAwal = 0) {
     this.saldo = saldoAwal;
   }
 }
-
-// Menangani event 'close' untuk menampilkan custom message
-rl.on("close", () => {
-  console.log("Input sudah selesai, interface ditutup.");
-});
-
-module.exports = { Bank, rl };
