caaf1a24c2dc212c72f3334049ec1d8bc5e0fab6
Yogihafidh <hafidhyogi@gmail.com>
2024-09-27 20:59:29 +0700

    feat(bank): create Bank class deposit

--- F-BEE24001186-km7-YHM-bankingsystem-ch2/bank_account.js
+++ F-BEE24001186-km7-YHM-bankingsystem-ch2/bank_account.js
@@ -8,6 +8,28 @@ class Bank {
   constructor(saldoAwal = 0) {
     this.saldo = saldoAwal;
   }
+
+  deposit() {
+    return new Promise((resolve, reject) => {
+      rl.question("Masukkan nominal yang ingin ditambahkan: ", (input) => {
+        const nominal = Number(input);
+
+        setTimeout(() => {
+          if (isNaN(nominal) || nominal <= 0) {
+            return reject(
+              new Error(
+                "Error: Nominal yang dimasukkan tidak valid. Harus berupa angka positif."
+              )
+            );
+          }
+          this.saldo += nominal;
+          resolve(
+            `Saldo berhasil ditambahkan. Saldo anda sekarang ${this.saldo}`
+          );
+        }, 3000);
+      });
+    });
+  }
 }
 
 // Menangani event 'close' untuk menampilkan custom message
