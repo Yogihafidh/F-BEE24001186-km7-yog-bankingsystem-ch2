458341c47079f5bf2762d35406b4edc55a79a57c
Yogihafidh <hafidhyogi@gmail.com>
2024-09-27 21:19:03 +0700

    refactor/add withdraw and deposit feature

--- F-BEE24001186-km7-YHM-bankingsystem-ch2/bank_account.js
+++ F-BEE24001186-km7-YHM-bankingsystem-ch2/bank_account.js
@@ -8,6 +8,57 @@ class Bank {
   constructor(saldoAwal = 0) {
     this.saldo = saldoAwal;
   }
+
+  deposit() {
+    return new Promise((resolve, reject) => {
+      rl.question("Masukkan nominal yang ingin ditambahkan: ", (input) => {
+        const nominal = Number(input);
+
+        setTimeout(() => {
+          if (isNaN(nominal) || nominal <= 0) {
+            return reject(
+              new Error(
+                "Error: Nominal yang dimasukkan tidak valid. Harus berupa angka positif."
+              )
+            );
+          }
+          this.saldo += nominal;
+          resolve(
+            `Saldo berhasil ditambahkan. Saldo anda sekarang ${this.saldo}`
+          );
+        }, 3000);
+      });
+    });
+  }
+
+  withdraw() {
+    return new Promise((resolve, reject) => {
+      rl.question("Masukkan nominal yang ingin dikurangi: ", (input) => {
+        const nominal = Number(input);
+
+        setTimeout(() => {
+          if (isNaN(nominal) || nominal <= 0) {
+            return reject(
+              new Error(
+                "Error: Nominal yang dimasukkan tidak valid. Harus berupa angka positif."
+              )
+            );
+          }
+          if (this.saldo < nominal) {
+            return reject(
+              new Error(
+                `Error: Saldo tidak mencukupi. Saldo anda saat ini ${this.saldo}.`
+              )
+            );
+          }
+          this.saldo -= nominal;
+          resolve(
+            `Saldo berhasil dikurangi. Saldo anda sekarang ${this.saldo}`
+          );
+        }, 3000);
+      });
+    });
+  }
 }
 
 // Menangani event 'close' untuk menampilkan custom message
